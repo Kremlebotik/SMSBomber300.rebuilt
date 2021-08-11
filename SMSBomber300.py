93893e7ef92d049511043c863285dc27ef309971
Kremlebotik <87243304+Kremlebotik@users.noreply.github.com>
2021-08-11 16:06:32 +0300

    Add files via upload

---
+++ SMSBomber300.py
@@ -0,0 +1,21 @@
+
+#   Version 12.1
+import requests, random, datetime, sys, time, argparse, os , colorama ,pickle
+from colorama import *
+from Banners import *
+from ServesSMS import *
+from Message import *
+from Function import *
+init()
+
+os.system("title SMS-Bomber-300")
+version = 'Бета'
+
+#Баннер cпамера
+time.sleep(1.8)
+OSystem()
+start()
+
+
+
+
